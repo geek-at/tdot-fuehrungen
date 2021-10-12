95beeca476dc00731fda6831cd9c4f6a73d282f6
Christian Haschek <christian@haschek.at>
2021-10-12 23:06:02 +0200

    no email. just google and phone

--- js/tdot.js
+++ js/tdot.js
@@ -179,10 +179,10 @@ function login_firebase(firebaseConfig) {
                 signInSuccessUrl: '/',
                 signInOptions: [
                     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                    {
+                    /*{
                         provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
                         signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
-                    },
+                    },*/
                     {
                         provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
                         defaultCountry: 'AT'

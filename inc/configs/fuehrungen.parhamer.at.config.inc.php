14910110c06b2342c25b241ad9929b4f6cd5ea7d
Christian Haschek <christian@haschek.at>
2021-10-12 15:37:16 +0200

    new configs

---
+++ inc/configs/fuehrungen.parhamer.at.config.inc.php
@@ -0,0 +1,60 @@
+<?php
+
+
+// firebase
+define('FIREBASE_APIKEY','AIzaSyDcVy4T0g-A_PqDvenN9rPy32cR4wI2Xbk');
+define('FIREBASE_AUTHDOMAIN','parhamer-tdot.firebaseapp.com');
+define('FIREBASE_DATABASEURL','https://parhamer-tdot.firebaseio.com');
+define('FIREBASE_PROJECT_ID','parhamer-tdot');
+define('FIREBASE_STORAGEBUCKET','parhamer-tdot.appspot.com');
+define('FIREBASE_MESSAGINGSENDERID','764917948690');
+define('FIREBASE_APPID','1:764917948690:web:5fe458a572d709de9213cb');
+
+//school
+define('ADMIN_PW',"7lfmcse0kx");
+define('SCHOOLNAME','Discover Parhamer');
+define('TITLE','Info Nachmittage am Parhamergymnasium');
+define('SCHOOL_INFOTEXT','Schüler und Schülerinnen des Parhamergymnasiums führen an zwei Nachmittagen in Kleingruppen durchs Schulhaus. Ein Tag der offenen Tür findet in diesem Herbst leider nicht statt.');
+define('SCHOOL_CONTACT','');
+define('PAGE_URL','https://fuehrungen.parhamer.at');
+define('PAGE_HEADER_IMG','/imgs/parhamer-h.jpg');
+define('PAGE_HEADER_CARD1','/imgs/grg17-1.jpg');
+define('PAGE_HEADER_CARD2','/imgs/grg17-2.jpg');
+
+define('EVENT_DATE','16. Nov. 2021');
+define('APPOINTMENT_MINUTES',15);
+define('EVENT_MAXRES_PER_TIMESLOT',5);
+define('PLATFORM_ONLINE_FROM',strtotime("21-10-2020"));
+define('PLATFORM_ONLINE_TO',strtotime("11-11-2021"));
+define('RESERVATION_EXPIRE_SECONDS',3600);
+
+//redis
+define('REDIS_SERVER', '127.0.0.1');
+define('REDIS_PORT', 6379);
+define('REDIS_PASS','');
+define('REDIS_PRESTRING', 'tdot:parhamer');
+
+//fields
+function getFields(){
+    return array(
+        'person1'=>     array('mandatory'=>true,'text'=>'Name 1. Person',   'hint'=>'Name der zum Besuch angemeldeten Person'),
+        'person2'=>     array('mandatory'=>false,'text'=>'Name 2. Person',  'hint'=>'Name des Kindes (Nur 1 Erwachsener + 1 Kind erlaubt)'),
+        'phone'=>       array('mandatory'=>true,'text'=>'Telefonnummer' ,   'hint'=>'Handy oder Festnetz für etwaige Kontaktaufnahme'),
+        // optional für welchen zweig interessiert
+        'email'=>       array('mandatory'=>true,'text'=>'Email Adresse')
+    );}
+
+//timeslots
+function getDayData() //16. nov 8:30-13:30
+{
+    $days[] = array('timeslots'=>array(
+        '08:00','08:15','08:30','08:45',
+        '09:00','09:15','09:30','09:45',
+        '10:00','10:15','10:30','10:45',
+        '11:00','11:15','11:30','11:45',
+        '12:00','12:15','12:30','12:45',
+        '13:00','13:15','13:30'
+    ),   'tag' => 'Dienstag, 16.11.2021');
+    return $days;
+}
+
---
+++ inc/configs/localhost.config.inc.php
@@ -0,0 +1,77 @@
+<?php
+
+
+// firebase
+define('FIREBASE_APIKEY','AIzaSyCSXGq6mj2QiEgmYpJklaFZ-fL4WZhQuBc');
+define('FIREBASE_AUTHDOMAIN','bg18-tdot.firebaseapp.com');
+define('FIREBASE_DATABASEURL','https://bg18-tdot.firebaseio.com');
+define('FIREBASE_PROJECT_ID','bg18-tdot');
+define('FIREBASE_STORAGEBUCKET','bg18-tdot.appspot.com');
+define('FIREBASE_MESSAGINGSENDERID','376752977671');
+define('FIREBASE_APPID','1:376752977671:web:97aa994e2e20b0a430b7a2');
+
+//school
+define('ADMIN_PW',"rGQzVDbBc");
+define('SCHOOLNAME','BG18 Klostergasse');
+define('TITLE','Schulführungen am BG18 Klostergasse');
+define('SCHOOL_INFOTEXT','Schüler und Schülerinnen des BG18 Klostergasse führen in Kleingruppen durchs Schulhaus.<br/><strong class="font-weight-bold">Wir bitten um Pünktlichkeit</strong>, da es sonst zu organisatorischen Problemen kommt.');
+define('SCHOOL_CONTACT','');
+define('PAGE_URL','https://tdot.bg18.at');
+define('PAGE_HEADER_IMG','/imgs/bg18-h.jpg');
+define('PAGE_HEADER_CARD1','/imgs/card-h.jpg');
+define('PAGE_HEADER_CARD2','/imgs/card-h.jpg');
+
+define('EVENT_DATE','16. März 2020');
+define('APPOINTMENT_MINUTES',5);
+define('APPOINTMENT_TOMINUTE',25);
+define('EVENT_MAXRES_PER_TIMESLOT',1);
+define('PLATFORM_ONLINE_FROM',strtotime("29-09-2020 17:58"));
+define('PLATFORM_ONLINE_TO',strtotime("29-10-2020 17:34")-1);
+define('RESERVATION_EXPIRE_SECONDS',3600);
+
+//redis
+define('REDIS_SERVER', '127.0.0.1');
+define('REDIS_PORT', 6379);
+define('REDIS_PASS','');
+define('REDIS_PRESTRING', 'tdot:bg18');
+
+//fields
+function getFields(){
+    return array(
+        'person1'=>     array('mandatory'=>true,'text'=>'Name 1. Person',   'hint'=>'Name der zum Besuch angemeldeten Person'),
+        'person2'=>     array('mandatory'=>false,'text'=>'Name 2. Person',  'hint'=>'Name der zweiten Person'),
+        'phone'=>       array('mandatory'=>true,'text'=>'Telefonnummer' ,   'hint'=>'Handy oder Festnetz für etwaige Kontaktaufnahme'),
+        'email'=>       array('mandatory'=>true,'text'=>'Email Adresse')
+    );}
+
+//timeslots
+function getDayData()
+{
+    $days[] = array(
+        'timeslots'=>getCustomTimeslots('10:00','12:50'),
+        'tag' => 'Dienstag, 01.12.2020'
+    );
+
+    $days[] = array(
+        'timeslots'=>getCustomTimeslots('09:00','11:50'),
+        'tag' => 'Freitag, 04.12.2020'
+    );
+
+    $days[] = array(
+        'timeslots'=>getCustomTimeslots('09:00','11:50'),
+        'tag' => 'Freitag, 11.12.2020'
+    );
+
+    $days[] = array(
+        'timeslots'=>getCustomTimeslots('10:00','12:50'),
+        'tag' => 'Dienstag, 15.12.2020'
+    );
+
+    $days[] = array(
+        'timeslots'=>getCustomTimeslots('09:00','11:50'),
+        'tag' => 'Freitag, 18.12.2020'
+    );
+
+    return $days;
+}
+

d7cc95aa3c43c233b8fb2a79f656e95e651a91fd
Christian Haschek <christian@haschek.at>
2021-10-12 18:09:41 +0200

    bg 18 update

--- inc/configs/fuehrungen.bg18.at.config.inc.php
+++ inc/configs/fuehrungen.bg18.at.config.inc.php
@@ -21,12 +21,12 @@ define('PAGE_HEADER_IMG','/imgs/bg18-h.jpg');
 define('PAGE_HEADER_CARD1','/imgs/card-h.jpg');
 define('PAGE_HEADER_CARD2','/imgs/card-h.jpg');
 
-define('EVENT_DATE','16. März 2020');
-define('APPOINTMENT_MINUTES',5);
-define('APPOINTMENT_TOMINUTE',25);
-define('EVENT_MAXRES_PER_TIMESLOT',1);
-define('PLATFORM_ONLINE_FROM',strtotime("29-09-2020 17:58"));
-define('PLATFORM_ONLINE_TO',strtotime("29-11-2020 17:34")-1);
+define('EVENT_DATE','2021/2022');
+define('APPOINTMENT_MINUTES',10);
+define('APPOINTMENT_TOMINUTE',20);
+define('EVENT_MAXRES_PER_TIMESLOT',3);
+define('PLATFORM_ONLINE_FROM',strtotime("12.10-2021"));
+define('PLATFORM_ONLINE_TO',strtotime("14-01-2022")-1);
 define('RESERVATION_EXPIRE_SECONDS',3600);
 
 //redis
@@ -48,28 +48,18 @@ function getFields(){
 function getDayData()
 {
     $days[] = array(
-        'timeslots'=>getCustomTimeslots('10:00','12:50'),
-        'tag' => 'Dienstag, 01.12.2020'
+        'timeslots'=>getCustomTimeslots('09:00','11:50'),
+        'tag' => 'Dienstag, 12. Nov. 2021'
     );
 
     $days[] = array(
         'timeslots'=>getCustomTimeslots('09:00','11:50'),
-        'tag' => 'Freitag, 04.12.2020'
+        'tag' => 'Freitag, 10. Dez. 2021'
     );
 
     $days[] = array(
         'timeslots'=>getCustomTimeslots('09:00','11:50'),
-        'tag' => 'Freitag, 11.12.2020'
-    );
-
-    $days[] = array(
-        'timeslots'=>getCustomTimeslots('10:00','12:50'),
-        'tag' => 'Dienstag, 15.12.2020'
-    );
-
-    $days[] = array(
-        'timeslots'=>getCustomTimeslots('09:00','11:50'),
-        'tag' => 'Freitag, 18.12.2020'
+        'tag' => 'Freitag, 14. Jän. 2022'
     );
 
     return $days;

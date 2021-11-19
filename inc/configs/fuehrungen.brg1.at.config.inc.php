44a68d043779e6102c6bbd2ecdac967f74b3dbd3
Christian Haschek <christian@haschek.at>
2021-11-19 23:37:29 +0100

    message

--- inc/configs/fuehrungen.brg1.at.config.inc.php
+++ inc/configs/fuehrungen.brg1.at.config.inc.php
@@ -16,13 +16,16 @@ define('SCHOOLNAME','Lise Meitner Realgymnasium');
 define('TITLE','BRG1 Schottenbastei');
 define('SCHOOL_INFOTEXT','Tag der Offenen Tür - Schüler/innen führen in Gruppen durch das Schulhaus.');
 define('SCHOOL_CONTACT','');
-/*define('SCHOOL_NOTICE','
-Aufgrund der derzeit gültigen Covid19-Regelungen ist der Besichtigung unseres Schulgebäudes am Tag der Offenen Tür 2021 ausschließlich nach Voranmeldung und mit einer von Schüler*innen geführten Tour durch das Schulhaus möglich. Im Rahmen der Führung kann das Schulhaus besichtigt und Unterricht beobachtet werden. 
-Unsere Informationsständen, welche sich am Tag der Offenen Tür in der Fußgängerzone vor der Schule befinden werden, können auch ohne Voranmeldung besucht werden. Hier werden Ihnen von unseren Lehrer*innen, Schüler*innen und Eltern Informationen zu sämtlichen Themen, unsere Schule betreffend, geboten.
-Für das Betreten des Schulhauses gilt für alle Besucherinnen und Besucher die 3-G-Regel (Impfbestätigung, Nachweis über Genesung oder aktueller PCR-Test-Nachweis). Während des Aufenthaltes im Schulhaus ist das Tragen eines Mund-Nasen-Schutzes oder einer FFP2- Maske verpflichtend.
-Die Führungen finden in Gruppen mit maximal 6 Personen statt. Pro Person können jeweils drei Plätze (z.B. zwei Elternteile mit Kind) reserviert werden.
-Die Plattform zur Terminreservierung wird am Freitag, den 22.10. freigeschaltet. Bitte geben Sie bei der Reservierung  Ihre Telefonnummer und E-Mailadresse an, damit wir Sie gegebenenfalls kontaktieren können.
-');*/
+define('SCHOOL_NOTICE','
+Leider mussten wir den Tag der Offenen Tür absagen. Als Ersatz bieten wir 20-minütige
+Schulführungen am 13. Dez., 17.Dez., 19. Jän., 20. Jän. und 21. Jän. an. An den
+Schulführungen kann man ausschließlich nach Voranmeldung über unsere Plattform
+teilnehmen.<br/>
+Für das Betreten des Schulhauses gilt für alle Besucherinnen und Besucher die 3-G-Regel
+(Impfbestätigung, Nachweis über Genesung oder aktueller PCR-Test-Nachweis). Während
+des Aufenthaltes im Schulhaus ist das Tragen eines Mund-Nasen-Schutzes (Kinder) oder
+einer FFP2- Maske (Eltern) verpflichtend.
+');
 define('PAGE_URL','https://fuehrungen.brg1.at');
 define('PAGE_HEADER_IMG','/imgs/brg1-h.jpg');
 define('PAGE_HEADER_CARD1','/imgs/grg17-1.jpg');
